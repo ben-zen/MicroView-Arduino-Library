10d5e443cfa041d966a164b147c46a61d4e59abd
ben-zen <benjf5+github@gmail.com>
2014-09-22 23:55:51 -0700

    There's still an issue with XOR painting, but my circle_fill_alt() shows about a 25% speed-up over the original microview one.

--- MicroView.cpp
+++ MicroView.cpp
@@ -596,11 +596,13 @@ void MicroView::circle(uint8_t x0, uint8_t y0, uint8_t radius, uint8_t color, ui
 
 /** \brief Draw filled circle.
 
-	Draw filled circle with radius using current fore color and current draw mode at x,y of the screen buffer.
+	Draw filled circle with radius using current fore color and current draw
+	mode at x,y of the screen buffer.
 */
 void MicroView::circleFill(uint8_t x0, uint8_t y0, uint8_t radius) {
 	circleFill(x0,y0,radius,foreColor,drawMode);
 }
+
 
 /** \brief Draw filled circle with color and mode.
 
@@ -650,15 +652,18 @@ void MicroView::circleFill(uint8_t x0, uint8_t y0, uint8_t radius, uint8_t color
 
 void MicroView::circle_fill_alt(uint8_t x0, uint8_t y0, uint8_t radius, uint8_t
                                 color, uint8_t mode){
-  int x = radius;
-  int y = 0;
-  int radiusError = 1 - x;
-  int y_last = y0 + x;
+  int8_t x = radius;
+  int8_t y = 0;
+  int8_t radiusError = 1 - x;
+  int8_t y_last = y0 + x;
+  int8_t y_low;
+  int8_t x_alt;
+  int8_t x_alt_max;
   while (x >= y) {
     pixel(x + x0, y + y0, color, mode);
     pixel(x0 - x, y + y0, color, mode);
-    int x_alt = (x0 - x) + 1;
-    int x_alt_max = x0 + x;
+    x_alt = (x0 - x) + 1;
+    x_alt_max = x0 + x;
     while (x_alt < x_alt_max) { // Fill the line between these two points
       pixel(x_alt, y + y0, color, mode);
       x_alt++;
@@ -680,7 +685,7 @@ void MicroView::circle_fill_alt(uint8_t x0, uint8_t y0, uint8_t radius, uint8_t
         x_alt = x0 - y + 1;
         x_alt_max = x0 + y;
         y_last = y0 + x;
-        int y_low = y0 - x;
+        y_low = y0 - x;
         while (x_alt < x_alt_max) {
           pixel(x_alt, y_last, color, mode);
           pixel(x_alt, y_low, color, mode);
@@ -1246,7 +1251,7 @@ void MicroView::doCmd(uint8_t cmdCount) {
 				Serial.print(serCmd[2]);
 				Serial.print(" ");
 				Serial.println(serCmd[3]);
-				circleFill(serCmd[1], serCmd[2], serCmd[3]);
+				//circleFill(serCmd[1], serCmd[2], serCmd[3]);
 				display();
 			} else if (cmdCount==5) {
 				Serial.print("circle ");
@@ -1259,7 +1264,7 @@ void MicroView::doCmd(uint8_t cmdCount) {
 				Serial.print(serCmd[4]);
 				Serial.print(" ");
 				Serial.println(serCmd[5]);
-				circleFill(serCmd[1], serCmd[2], serCmd[3], serCmd[4], serCmd[5]);
+				//circleFill(serCmd[1], serCmd[2], serCmd[3], serCmd[4], serCmd[5]);
 				display();
 			}
 			break;
